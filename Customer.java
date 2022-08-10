32d80368cdb857822debdb4ab9929adc0118da59
MathVasconcelos <matheuscostasantos@live.com>
2022-08-10 16:30:40 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -42,6 +42,8 @@ public class Customer {
                  thisAmount += (each.getDaysRented() - 3) * 1.5;
                break;
         }
+        // Chamada do novo método criado
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -60,5 +62,24 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
+   }
+   private double amountFor(Rental each) {
+      //Adicionar o trecho de código extraído.
+      //determine amounts for each line
+      switch (each.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2)
+               thisAmount += (each.getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3)
+               thisAmount += (each.getDaysRented() - 3) * 1.5;
+             break;
+      }
    }
 }
