e18def1707f13e0c1613a0397f987b2f00d0cee6
MathVasconcelos <matheuscostasantos@live.com>
2022-08-10 19:09:28 -0300

    COMMIT 9

--- Rental.java
+++ Rental.java
@@ -11,6 +11,7 @@ public class Rental {
    public Movie getMovie() {
       return _movie;
    }
+
    public double getCharge() {
       double thisAmount = 0;
       switch (getMovie().getPriceCode()) {
@@ -30,19 +31,17 @@ public class Rental {
     }
     return thisAmount;
 
+      return _movie.getCharge(_daysRented);
    }
 
    public int getFrequentRenterPoints() {
       int frequentRenterPoints = 0;
-
       // add frequent renter points
       frequentRenterPoints ++;
       // add bonus for a two day new release rental
       if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
          this.getDaysRented() > 1) frequentRenterPoints ++;
-
-
+         
       return frequentRenterPoints;
-
    }
 }
